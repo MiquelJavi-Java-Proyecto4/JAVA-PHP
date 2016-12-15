b4f2b3748e62cd842e08afa03cb7f5f51e4d6350
MiquelGomez <100005548.joan23@fje.edu>
2016-12-15 17:05:27 +0100

    login funciona

--- Silla/src/controlador/ProductoController.java
+++ Silla/src/controlador/ProductoController.java
@@ -5,10 +5,49 @@
  */
 package controlador;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modelo.Conexion;
+import modelo.User;
+
 /**
  *
  * @author Javier
  */
 public class ProductoController {
+
+    public ProductoController() {
+    }
+    
+    public int login(User usuario){
+     int result = 0;
+     Conexion conectar = new Conexion();
+     Connection cn = conectar.conexion();
+     PreparedStatement pst =null;
+     ResultSet rs = null;
+     
+     String sql = "SELECT * FROM tbl_users WHERE usu_nombre = ? AND usu_password = ?";
+        try {
+            pst = cn.prepareStatement(sql);
+            pst.setString(1, usuario.getNombre());
+            pst.setString(2, usuario.getApellido());
+            rs = pst.executeQuery();
+            if (!rs.next()){
+               int b = 1; 
+               return b;
+            } else {
+               int b=2;
+               return b;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductoController.class.getName()).log(Level.SEVERE, null, ex);
+            int b = 3;
+            return b;
+        }
+    }
     
 }
