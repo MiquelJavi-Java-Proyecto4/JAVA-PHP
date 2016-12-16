81a762414c2a36279d5e66d8707cd23fb03aa444
jAcebo <35202.joan23@fje.edu>
2016-12-16 16:03:27 +0100

    vistaProducto a VistaProducto

--- Silla/src/vista/vistaProducto.java
+++ Silla/src/vista/vistaProducto.java
@@ -327,6 +327,8 @@ public class VistaProducto extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
